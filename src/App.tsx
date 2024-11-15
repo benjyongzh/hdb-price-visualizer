462861be4a4e5b90813e01a6951da72ccfda46ad
benjyongzh <benj_yongzh@hotmail.com>
2024-11-15 14:55:57 +0800

    Add accordion for filter section that can be expanded

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,25 @@ import {
 import { Slider } from "@/components/ui/slider";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import { CircleHelp } from "lucide-react";
+import { CircleHelp, SlidersHorizontal } from "lucide-react";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { Separator } from "@/components/ui/separator";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+} from "@/components/ui/accordion";
 
 import Map from "./components/Map";
 import { FilterButton, FilterButtonSkeleton } from "./components/FilterButton";
 import { getRandomIntInclusive, formatMoneyString } from "./lib/utils";
 import { GeoJsonFeature, GeoJsonData } from "@/lib/types";
+import apiService from "./services/apiService";
 
-const url: string = import.meta.env.VITE_API_URL + "/polygons/latest-avg/";
 const sliderDefaultvalue: number = 400000;
 const sliderMinValue: number = 50000;
 const sliderMaxValue: number = 2000000;
@@ -37,9 +43,10 @@ const initialGeoJsonData: GeoJsonData = {
 function App() {
   const [geojsonData, setGeojsonData] =
     useState<GeoJsonData>(initialGeoJsonData);
-  // const [flatTypes, setFlatTypes] = useState<Array<string>>([]);
-  // const [loadingFlatTypes, setLoadingFlatTypes] = useState<boolean>(false);
+  const [flatTypes, setFlatTypes] = useState<Array<string> | string>([]);
+  const [loadingFlatTypes, setLoadingFlatTypes] = useState<boolean>(false);
   const [sliderValue, setSliderValue] = useState<number>(sliderDefaultvalue);
+  const [filterIsOpen, setFilterIsOpen] = useState<boolean>(false);
 
   const [minPrice, setMinPrice] = useState<number>(0);
   const [maxPrice, setMaxPrice] = useState<number>(1);
@@ -61,7 +68,7 @@ function App() {
 
   // Fetch geojson data stream
   const fetchData = async () => {
-    const response = await fetch(url); // Your Django API endpoint
+    const response = await apiService.getLatestAvg(); // Your Django API endpoint
     const reader = response.body?.getReader();
     const decoder = new TextDecoder("utf-8");
 
@@ -120,12 +127,26 @@ function App() {
         setFlatTypes(data.results); // Update map with new data
         setLoadingFlatTypes(false);
       } catch (error) {
-        console.error("Error getting flat types:", error);
+        console.log("Error getting Flat Types:", error);
+        setFlatTypes("Error getting flat Types"); // Update map with new data
         setLoadingFlatTypes(false);
       }
     };
 
     getFlatTypes();
+  }, []);
+
+  // get all geojsondata without any properties yet. to show all the flats first
+  useEffect(() => {
+    const getBaseGeojsonPolygons = async () => {
+      try {
+        const getBaseGeojsonData = await apiService.getBaseGeosjonPolygons();
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    // getBaseGeojsonPolygons();
   }, []);
 
   useEffect(() => {
@@ -150,53 +171,83 @@ function App() {
           <CardHeader>
             <CardTitle>HDB Resale Price Data</CardTitle>
             <CardDescription>
-              Select and filter data to visualize
+              Select and filter data to visualize past resale transactions
             </CardDescription>
           </CardHeader>
           <CardContent className="flex flex-col gap-4">
-            <section className="flex flex-col gap-4 justify-between">
-              <Label
-                htmlFor="budget"
-                className="flex items-center justify-between"
+            <section className="flex items-center min-h-11 h-11">
+              <div className="flex flex-col gap-2 justify-between w-full">
+                <Label
+                  htmlFor="budget"
+                  className="flex items-center justify-between"
+                >
+                  <div className="flex items-center gap-2">
+                    Budget
+                    <Popover>
+                      <PopoverTrigger>
+                        <Button size="icon" variant="ghost" className="w-7 h-7">
+                          <CircleHelp color="hsl(217, 0%, 90%)" size={16} />
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent side="top" className="text-center">
+                        Set your maximum resale price
+                      </PopoverContent>
+                    </Popover>
+                  </div>
+
+                  <p className="text-right">
+                    $ {formatMoneyString(sliderValue)}
+                  </p>
+                </Label>
+                <Slider
+                  defaultValue={[sliderDefaultvalue]}
+                  min={sliderMinValue}
+                  max={sliderMaxValue}
+                  onValueChange={([value]) => setSliderValue(value)}
+                  name="budget"
+                  step={1000}
+                />
+              </div>
+              <Separator orientation="vertical" className="ml-4 mr-2" />
+              <Button
+                onClick={() => setFilterIsOpen(!filterIsOpen)}
+                variant="ghost"
+                className={`aspect-square p-0 m-0 ${
+                  filterIsOpen ? "bg-accent text-accent-foreground" : ""
+                }`}
               >
-                <div className="flex items-center gap-2">
-                  Budget
-                  <Popover>
-                    <PopoverTrigger>
-                      <Button size="icon" variant="ghost" className="w-7 h-7">
-                        <CircleHelp color="hsl(217, 0%, 90%)" size={16} />
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent side="top" className="text-center">
-                      Set your maximum resale price
-                    </PopoverContent>
-                  </Popover>
-                </div>
-
-                <p className="text-right">$ {formatMoneyString(sliderValue)}</p>
-              </Label>
-              <Slider
-                defaultValue={[sliderDefaultvalue]}
-                min={sliderMinValue}
-                max={sliderMaxValue}
-                onValueChange={([value]) => setSliderValue(value)}
-                name="budget"
-                step={1000}
-              />
+                <SlidersHorizontal />
+              </Button>
             </section>
+            <Accordion
+              type="single"
+              collapsible
+              value={filterIsOpen ? "filter-section" : undefined}
+            >
+              <AccordionItem value="filter-section">
+                <AccordionContent>
+                  <Separator className="mb-4" />
+                  <section className="flex flex-col gap-2 justify-start">
+                    <h2>Filters</h2>
+                    <section className="flex gap-2 flex-wrap">
+                      {loadingFlatTypes
+                        ? Array.from({ length: 6 }).map((_item, index) => (
+                            <FilterButtonSkeleton
+                              key={index}
+                              length={getRandomIntInclusive(60, 150)}
+                            />
+                          ))
+                        : Array.isArray(flatTypes)
+                        ? flatTypes.map((type) => (
+                            <FilterButton filterCategory={type} key={type} />
+                          ))
+                        : flatTypes}
+                    </section>
+                  </section>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
 
-            {/* <section className="flex gap-2 flex-wrap">
-              {loadingFlatTypes
-                ? Array.from({ length: 6 }).map((_item, index) => (
-                    <FilterButtonSkeleton
-                      key={index}
-                      length={getRandomIntInclusive(60, 150)}
-                    />
-                  ))
-                : flatTypes.map((type) => (
-                    <FilterButton filterCategory={type} key={type} />
-                  ))}
-            </section> */}
             <Button onClick={() => fetchData()}>Filter</Button>
           </CardContent>
         </Card>
